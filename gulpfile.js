2cdf8c899e59c716af377ef2ae1d75c720cf14d3
Mr.Hope <zhangbowang1998@gmail.com>
2021-02-23 18:50:08 +0800

    fix: update scss build

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,9 @@
 const { dest, parallel, src, watch } = require("gulp");
 const rename = require("gulp-rename");
-const sass = require("gulp-sass");
+const { gulpSass } = require("@mr-hope/gulp-sass");
 const PluginError = require("plugin-error");
-const through = require("through2");
 const typescript = require("gulp-typescript");
-
-sass.compiler = require("sass");
+const { Transform } = require("stream");
 
 const appTSProject = typescript.createProject("tsconfig.app.json");
 const cloudTSProject = typescript.createProject("tsconfig.cloud.json");
@@ -13,42 +11,45 @@ const cloudTSProject = typescript.createProject("tsconfig.cloud.json");
 const buildWXSS = () =>
   src("app/**/*.scss")
     .pipe(
-      sass({
-        outputStyle: "expanded",
-        // hack for remaining '@import'
-        importer: (url, _prev, done) => {
+      gulpSass({
+        // use `@` as hack for remaining '@import'
+        importer: (url) => {
           if (url.includes(".css")) return null;
-          done({ contents: `@import "${url}.css"` });
+
+          return { contents: `@import "${url}.css"` };
         },
-      }).on("error", sass.logError)
+      }).on("error", gulpSass.logError)
     )
     .pipe(rename({ extname: ".wxss" }))
     .pipe(
-      through.obj(function (file, enc, cb) {
-        if (file.isNull()) {
-          this.push(file);
+      new Transform({
+        objectMode: true,
+        transform(chunk, _enc, callback) {
+          if (chunk.isNull()) {
+            this.push(chunk);
 
-          return cb();
-        }
+            return callback();
+          }
 
-        if (file.isStream()) {
-          this.emit(
-            "error",
-            new PluginError("Sass", "Streaming not supported")
-          );
+          if (chunk.isStream()) {
+            this.emit(
+              "error",
+              new PluginError("Sass", "Streaming not supported")
+            );
 
-          return cb();
-        }
+            return callback();
+          }
 
-        const content = file.contents
-          .toString()
-          .replace(/@import url\((.*?)\.css\)/gu, '@import "$1.wxss"');
+          const content = chunk.contents
+            .toString()
+            .replace(/@import "@(.*?)\.css"/gu, '@import "$1.wxss"');
 
-        file.contents = Buffer.from(content);
+          chunk.contents = Buffer.from(content);
 
-        this.push(file);
+          this.push(chunk);
 
-        cb();
+          callback();
+        },
       })
     )
     .pipe(dest("dist/app"));
@@ -75,7 +76,7 @@ const watchCloudTypescript = () =>
 
 const watchAppFiles = () =>
   watch(
-    "app/**/*.{wxml,wxs,json,svg,png}",
+    "app/**/*.{wxml,wxs,json,svg,png,webp}",
     { ignoreInitial: false },
     moveAppFiles
   );
